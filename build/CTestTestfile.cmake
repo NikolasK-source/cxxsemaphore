e6bfa9aa6e28373982306f893533fdf04697069d
Nikolas Koesling <nikolas@koesling.info>
2023-12-15 18:35:52 +0100

    remove build dir

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nk/Development/Privat/_CPP/cxxsemaphore
-# Build directory: /home/nk/Development/Privat/_CPP/cxxsemaphore/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("include")
-subdirs("test")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nk/Development/Privat/_CPP/cxxsemaphore/src
-# Build directory: /home/nk/Development/Privat/_CPP/cxxsemaphore/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
